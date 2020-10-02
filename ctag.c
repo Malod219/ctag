313edc7e8b0f0e61b9b0112ab208b1f9cb7ab083
RisingThumb <aaronleonard@risingthumb.xyz>
2020-10-02 13:59:59 +0100

    Directory/File Browser complete

--- ctag.c
+++ ctag.c
@@ -3,14 +3,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <unistd.h>
 
 #define CURSOR_INVIS    0
 /* We get keys correspond to ASCII integer value */
 #define KEY_QUIT    0x71
 #define KEY_TAB     0x09
 
+#define DIRECTORYLINES 1000
+#define MAXDIRWIDTH 100
+#define STRBUFF 66
+#define STRBUFFET 200
+
+char dirlines[DIRECTORYLINES][MAXDIRWIDTH];
 int g_row;
 int g_col;
+int d_id = 0;
+int d_sel_id = 0;
+int d_size = 0;
 
 typedef struct windowData {
     WINDOW* window;
@@ -44,6 +54,7 @@ void render(void);
 void drawwindow(windowData *wd);
 void initialisecolors(void);
 void getdirectoryinfo();
+void drawdirectory(windowData *wd);
 
 int main( int argc, char *argv[]) {
     int ch;
@@ -71,15 +82,32 @@ int main( int argc, char *argv[]) {
     create_newwin(g_row-2, g_col/2, 1, g_col/2, 1, "-Files-", edit, &editwin);
     create_newwin(1, g_col, 0, (g_col-strlen(toptext))/2, 0, toptext, never, &panwin);
     create_newwin(1, g_col, g_row-1, (g_col-strlen(bottomtext))/2, 0, bottomtext, never, &panwinbottom);
-
+    getdirectoryinfo();
     for(render(); tolower(ch = getch()) != KEY_QUIT; render()){
-        switch(ch) {
-            case KEY_TAB:
-                if (windows.state == edit)
-                    windows.state = dir;
-                else
+        if (windows.state == dir) {
+            switch(ch) {
+                case KEY_TAB:
                     windows.state = edit;
-                continue;
+                    continue;
+                case KEY_DOWN:
+                    if (d_sel_id < d_size - 1)
+                        d_sel_id += 1;
+                    continue;
+                case KEY_UP:
+                    if (d_sel_id > 0)
+                        d_sel_id -= 1;
+                    continue;
+                case 'o':
+                    chdir(dirlines[d_sel_id]);
+                    getdirectoryinfo();
+                    continue;
+            }
+        }
+        if (windows.state == edit) {
+            switch(ch) {
+                case KEY_TAB:
+                    windows.state = dir;
+            }
         }
     }
     endwin();
@@ -92,17 +120,22 @@ void getdirectoryinfo() {
     int width = getmaxx(windows.directory->window);
     char line[width];
     int i = 1;
+    int len;
 
     errno = system("ls -1 > /tmp/ctag-temp");
     if (errno != 0)
         return; /* todo: unfuck. Return error to user */
     fp = fopen("/tmp/ctag-temp", "r");
+    strcpy(dirlines[0], "..");
     while(fgets(line, sizeof(line), fp) != NULL) {
-        mvwprintw(windows.directory->window, i++, 1, line);
+        len = strlen(line);
+        if (line[len-1] == '\n')
+            line[len-1] = 0;
+        strcpy(dirlines[i++], line);
+        mvwprintw(windows.directory->window, i, 1, line);
     }
     fclose(fp);
-
-
+    d_size = i;
 }
 
 void initialisecolors() {
@@ -121,12 +154,27 @@ void create_newwin(int height, int width, int starty, int startx, int boxed, cha
 }
 
 void render() {
-    getdirectoryinfo();
+    wclear(windows.directory->window);
+    wclear(windows.editor->window);
+
+    drawdirectory(windows.directory);
     drawwindow(windows.directory);
 
     drawwindow(windows.editor);
     drawwindow(windows.toppanel);
     drawwindow(windows.bottompanel);
+}
+
+void drawdirectory(windowData *wd) {
+    int i = 0;
+    WINDOW* w = wd->window;
+
+    for(; i < d_size; i++) {
+        if (d_sel_id == i)
+            wattron(w, A_REVERSE);
+        mvwprintw(w, i+1, 1, dirlines[i]);
+        wattroff(w, A_REVERSE);
+    }
 }
 
 void drawwindow(windowData *wd) {
