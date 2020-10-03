167cb656fe15216d6c78b4a3e13f103d688fe299
RisingThumb <aaronleonard@risingthumb.xyz>
2020-10-03 22:50:17 +0100

    Better directory reading

--- Makefile
+++ Makefile
@@ -4,8 +4,7 @@ CURSES=-lncurses -lmenu -lform
 CURSESW=-lncursesw -lmenuw -lformw -D_CURSESW_WIDE
 FILES=ctag.c
 CFLAGS+=-std=c99 -pedantic -Wall
-CPPFLAGS+=-I/usr/local/include `taglib-config --cflags`
-LDFLAGS+=-L/usr/local/lib `taglib-config --libs` -ltag_c -lstdc++
+LDFLAGS+=-L/usr/local/lib -lstdc++
 
 all:
 	$(CC) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS) $(CURSES) $(FILES) -o $(PROG)
--- ctag.c
+++ ctag.c
@@ -4,29 +4,32 @@
 #include <string.h>
 #include <ctype.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 #define CURSOR_INVIS    0
 /* We get keys correspond to ASCII integer value */
 #define KEY_QUIT    0x71
 #define KEY_TAB     0x09
+#define KEY_ENT     0x0a
 
 #define DIRECTORYLINES 1000
-#define MAXDIRWIDTH 100
+#define MAXDIRWIDTH 256
 #define STRBUFF 66
 #define STRBUFFET 200
 
-char dirlines[DIRECTORYLINES][MAXDIRWIDTH];
-int g_row;
-int g_col;
-int d_id = 0;
-int d_sel_id = 0;
-int d_size = 0;
+char dirlines[DIRECTORYLINES][MAXDIRWIDTH]; // Consider a malloc approach, so it extends to any directory with size>1000
+char *filenameEditing;
 
 typedef struct windowData {
     WINDOW* window;
     int     boxed;
     char*   titleBar;
     int     state;
+    int     id; // Corresponds with position when scrolling up/down
+    int     sel_id; // Corresponds with selected element
+    int     size; // Size in number of elements
 } windowData;
 
 struct windows{
@@ -49,17 +52,22 @@ enum colorpairs {
     panel
 };
 
-void create_newwin(int height, int width, int starty, int startx, int boxed, char* title, int state, windowData *wd);
+void createNewWindow(int height, int width, int starty, int startx, int boxed, char* title, int state, windowData *wd);
 void render(void);
-void drawwindow(windowData *wd);
-void initialisecolors(void);
-void getdirectoryinfo();
-void drawdirectory(windowData *wd);
+void drawWindow(windowData *wd);
+void initialiseColors(void);
+void getDirectoryInfo(int *size);
+void drawDirectory(windowData *wd);
+int isRegularFile(char* filename);
+int compare(const void* pa, const void *pb);
 
 int main( int argc, char *argv[]) {
     int ch;
+    int row, col;
     char * toptext = "Made by RisingThumb          https://risingthumb.xyz ";
     char * bottomtext = "TAB switch menu    Q to quit";
+    int *sel_id;
+    int *size;
     windowData dirwin, editwin, panwin, panwinbottom;
 
     windows.directory = &dirwin;
@@ -74,32 +82,45 @@ int main( int argc, char *argv[]) {
     curs_set(CURSOR_INVIS);
     raw();
     start_color();
-    initialisecolors();
+    initialiseColors();
     refresh();
-    getmaxyx(stdscr, g_row, g_col);
-             /*  height, width,  y, x,          boxed,  title, windowData */
-    create_newwin(g_row-2, g_col/2, 1, 0, 1, "-Directory-", dir, &dirwin);
-    create_newwin(g_row-2, g_col/2, 1, g_col/2, 1, "-Files-", edit, &editwin);
-    create_newwin(1, g_col, 0, (g_col-strlen(toptext))/2, 0, toptext, never, &panwin);
-    create_newwin(1, g_col, g_row-1, (g_col-strlen(bottomtext))/2, 0, bottomtext, never, &panwinbottom);
-    getdirectoryinfo();
-    for(render(); tolower(ch = getch()) != KEY_QUIT; render()){
+    getmaxyx(stdscr, row, col);
+
+    /* Create windows accordingly */
+    createNewWindow(row-2, col/2, 1, 0, 1, "-Directory-", dir, &dirwin);
+    createNewWindow(row-2, col/2, 1, col/2, 1, "-Files-", edit, &editwin);
+    createNewWindow(1, col, 0, (col-strlen(toptext))/2, 0, toptext, never, &panwin);
+    createNewWindow(1, col, row-1, (col-strlen(bottomtext))/2, 0, bottomtext, never, &panwinbottom);
+
+    getDirectoryInfo(&windows.directory->size);
+    for(render(); (ch = tolower(getch())) != KEY_QUIT; render()){
         if (windows.state == dir) {
+            sel_id = &windows.directory->sel_id;
+            size = &windows.directory->size;
             switch(ch) {
                 case KEY_TAB:
                     windows.state = edit;
                     continue;
                 case KEY_DOWN:
-                    if (d_sel_id < d_size - 1)
-                        d_sel_id += 1;
+                    if ((*sel_id) < (*size) - 1)
+                        (*sel_id) += 1;
                     continue;
                 case KEY_UP:
-                    if (d_sel_id > 0)
-                        d_sel_id -= 1;
+                    if (*sel_id > 0)
+                        (*sel_id) -= 1;
                     continue;
-                case 'o':
-                    chdir(dirlines[d_sel_id]);
-                    getdirectoryinfo();
+                case KEY_ENT:
+                    wclear(windows.directory->window);
+                    wclear(windows.editor->window);
+                    if ( isRegularFile(dirlines[*sel_id]) ) {
+                        windows.state = edit;
+                        filenameEditing = dirlines[*sel_id];
+                    }
+                    else {
+                        if (chdir(dirlines[*sel_id]) == 0)
+                            (*sel_id) = 0;
+                        getDirectoryInfo(size);
+                    }
                     continue;
             }
         }
@@ -114,70 +135,72 @@ int main( int argc, char *argv[]) {
     return 0;
 }
 
-void getdirectoryinfo() {
-    int errno;
-    FILE *fp;
-    int width = getmaxx(windows.directory->window);
-    char line[width];
-    int i = 1;
-    int len;
-
-    errno = system("ls -1 > /tmp/ctag-temp");
-    if (errno != 0)
-        return; /* todo: unfuck. Return error to user */
-    fp = fopen("/tmp/ctag-temp", "r");
-    strcpy(dirlines[0], "..");
-    while(fgets(line, sizeof(line), fp) != NULL) {
-        len = strlen(line);
-        if (line[len-1] == '\n')
-            line[len-1] = 0;
-        strcpy(dirlines[i++], line);
-        mvwprintw(windows.directory->window, i, 1, line);
-    }
-    fclose(fp);
-    d_size = i;
+int isRegularFile(char* filename) {
+    struct stat file_stat;
+    stat(filename, &file_stat);
+    return S_ISREG(file_stat.st_mode);
 }
 
-void initialisecolors() {
+void getDirectoryInfo(int *size) {
+    int i = 0;
+    DIR *dir;
+    struct dirent *dp;
+
+    dir = opendir(".");
+    strcpy(dirlines[i++], "..");
+    while((dp = readdir(dir)) != NULL)
+        if (dp->d_name[0] != '.') {
+            strcpy(dirlines[i++], dp->d_name);
+            }
+    qsort(&dirlines, i, MAXDIRWIDTH, compare);
+    *size = i;
+}
+
+int compare(const void* pa, const void *pb) {
+    return strcmp(pa, pb);
+}
+
+void initialiseColors() {
     init_pair(normal, COLOR_WHITE, COLOR_BLACK);
     init_pair(highlight, COLOR_YELLOW, COLOR_BLACK);
     init_pair(panel, COLOR_BLUE, COLOR_BLACK);
 }
 
-void create_newwin(int height, int width, int starty, int startx, int boxed, char* title, int state, windowData *wd) {
+void createNewWindow(int height, int width, int starty, int startx, int boxed, char* title, int state, windowData *wd) {
     WINDOW *local_win;
     local_win = newwin(height, width, starty, startx);
     wd->window=local_win;
     wd->boxed=boxed;
     wd->titleBar=title;
     wd->state = state;
+    wd->id = 0;
+    wd->sel_id = 0;
+    wd->size = 0;
 }
 
 void render() {
-    wclear(windows.directory->window);
-    wclear(windows.editor->window);
 
-    drawdirectory(windows.directory);
-    drawwindow(windows.directory);
+    drawDirectory(windows.directory);
+    drawWindow(windows.directory);
 
-    drawwindow(windows.editor);
-    drawwindow(windows.toppanel);
-    drawwindow(windows.bottompanel);
+    drawWindow(windows.editor);
+    drawWindow(windows.toppanel);
+    drawWindow(windows.bottompanel);
 }
 
-void drawdirectory(windowData *wd) {
+void drawDirectory(windowData *wd) {
     int i = 0;
     WINDOW* w = wd->window;
 
-    for(; i < d_size; i++) {
-        if (d_sel_id == i)
+    for(; i < wd->size; i++) {
+        if (wd->sel_id == i)
             wattron(w, A_REVERSE);
         mvwprintw(w, i+1, 1, dirlines[i]);
         wattroff(w, A_REVERSE);
     }
 }
 
-void drawwindow(windowData *wd) {
+void drawWindow(windowData *wd) {
     WINDOW* w = wd->window;
     if (windows.state && windows.state == wd->state)
         wattron(w, COLOR_PAIR(highlight));
